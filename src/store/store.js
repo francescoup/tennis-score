a0889b2dc38416d7507c89d1928dc1320d92f478
francesco mangione <f.mangione@live.com>
2025-03-25 13:04:14 +0100

    fix:added netlify redirect

--- src/store/store.js
+++ src/store/store.js
@@ -61,8 +61,6 @@ export const useTennisScore = defineStore("TennisScore", () => {
       playerTwo.point = 0;
       playerOne.game++;
       checkForSetWinner(player);
-
-      // checkForSetWinner(player);
     } else if (
       (isTieBreack.value === true
         ? playerTwo.point >= 7
@@ -73,8 +71,6 @@ export const useTennisScore = defineStore("TennisScore", () => {
       playerTwo.point = 0;
       playerTwo.game++;
       checkForSetWinner(player);
-
-      // checkForSetWinner(player);
     } else if (
       isTieBreack.value === false &&
       playerOne.point >= 3 &&
@@ -86,9 +82,10 @@ export const useTennisScore = defineStore("TennisScore", () => {
       console.log("deuce");
     }
   };
-  //check for winner
+
+  // Verifica se un giocatore ha vinto un set (6 giochi con almeno 2 di vantaggio)
+
   const checkForSetWinner = (player) => {
-    // Verifica se un giocatore ha vinto un set (6 giochi con almeno 2 di vantaggio)
     if (
       isTieBreack.value === true
         ? player === 1 && playerOne.game === 7
@@ -129,11 +126,9 @@ export const useTennisScore = defineStore("TennisScore", () => {
     if (playerOne.sets === 2) {
       openModal.value = true;
       isWinner.value = playerOne.nome;
-      //resetScore();
     } else if (playerTwo.sets === 2) {
       openModal.value = true;
       isWinner.value = playerTwo.nome;
-      //resetScore();
     }
   };
   //store results
@@ -161,6 +156,8 @@ export const useTennisScore = defineStore("TennisScore", () => {
   const resetScore = () => {
     playerOne.nome = "";
     playerTwo.nome = "";
+    playerOne.point = 0;
+    playerTwo.point = 0;
     playerOne.game = 0;
     playerTwo.game = 0;
     playerOne.setOne = 0;
